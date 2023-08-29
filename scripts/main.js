66725240a9716c65a883c5efaeabf45be5b7e8f4
1dxrpz <scaphoidederps@gmail.com>
2023-08-30 02:19:15 +0300

    docking system

--- scripts/main.js
+++ scripts/main.js
@@ -1,60 +1,124 @@
-var nums = "0123456789", space = "          ";
-var colors = ["#fcc", "#f5f577", "#cfc", "#aff", "#ccf", "#fcf"];
-var rulers = [], value = "";
-for (var i = 1; i <= 1; i++) {
-    rulers.push({ color: colors[i], column: i * 10, lineStyle: "dashed" });
-    for (var j = 1; j < i; j++) value += space;
-        value += nums + "\n";
-}
+$(document).ready(function () {
+    var layout = [{
+        type: 'layoutGroup',
+        orientation: 'horizontal',
+        items: [{
+            type: 'autoHideGroup',
+            alignment: 'left',
+            width: 80,
+            unpinnedWidth: 200,
+            items: [{
+                type: 'layoutPanel',
+                title: 'Toolbox',
+                contentContainer: 'ToolboxPanel'
+            }, {
+                type: 'layoutPanel',
+                title: 'Help',
+                contentContainer: 'HelpPanel'
+            }]
+        }, {
+            type: 'layoutGroup',
+            orientation: 'vertical',
+            width: window.innerWidth - 200,
+            items: [{
+                type: 'documentGroup',
+                height: 400,
+                minHeight: 200,
+                items: [{
+                    type: 'documentPanel',
+                    title: 'Document 1',
+                    contentContainer: 'Document1Panel'
+                }, {
+                    type: 'documentPanel',
+                    title: 'Document 2',
+                    contentContainer: 'Document2Panel'
+                }]
+            }, {
+                type: 'tabbedGroup',
+                height: 200,
+                pinnedHeight: 30,
+                items: [{
+                    type: 'layoutPanel',
+                    title: 'Error List',
+                    contentContainer: 'ErrorListPanel'
+                }]
+            }]
+        }, {
+            type: 'tabbedGroup',
+            width: 220,
+            minWidth: 200,
+            items: [{
+                type: 'layoutPanel',
+                title: 'Solution Explorer',
+                contentContainer: 'SolutionExplorerPanel'
+            }, {
+                type: 'layoutPanel',
+                title: 'Properties',
+                contentContainer: 'PropertiesPanel'
+            }]
+        }]
+    }, {
+        type: 'floatGroup',
+        width: 500,
+        height: 200,
+        position: {
+            x: 350,
+            y: 250
+        },
+        items: [{
+            type: 'layoutPanel',
+            title: 'Output',
+            contentContainer: 'OutputPanel'
+        }]
+    }];
+    $('#jqxDockingLayout').jqxDockingLayout({ width: window.innerWidth, height: window.innerHeight - 20, layout: layout });
 
-CodeMirror.commands.autocomplete = function (cm) {
-    cm.showHint();
-}
 
-var editor = CodeMirror.fromTextArea(document.querySelector("#code_editor"), {
-    lineNumbers: true,
-    styleActiveLine: true,
-    rulers: rulers,
-    value: value + value + value,
-    smartIndent: true,
-    matchBrackets: true,
-    indentWithTabs: true,
-    indentUnit: 4,
-    tabSize: 4,
-    autofocus: true,
-    mode: "text/x-csrc",
-    viewportMargin: Infinity,
-    electricChars: true,
-    autoCloseBrackets: true,
-    lineWrapping: true,
-    extraKeys: {
-        "Ctrl-Q": function (cm) { cm.foldCode(cm.getCursor()); },
-        "Ctrl-Space": "autocomplete"
-    },
-    foldGutter: true,
-    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
-    theme: "shadowfox"
-});
-
-var input = document.getElementById("select");
-function selectTheme() {
-    var theme = input.options[input.selectedIndex].textContent;
-    editor.setOption("theme", theme);
-    location.hash = "#" + theme;
-}
-
-
-const code_area = document.querySelector(".code_area");
-const inspector = document.querySelector("#inspector");
-const gutter = document.querySelector(".gutter");
-var gutter_drag = false;
-
-window.addEventListener('mousemove', e => {
-    if (gutter_drag && e.clientX > 200 && e.clientX < 1000) {
-        inspector.style.width = `${e.clientX}px`;
-        code_area.style.width = `calc(100% - ${e.clientX}px)`;
+    var nums = "0123456789", space = "          ";
+    var colors = ["#fcc", "#f5f577", "#cfc", "#aff", "#ccf", "#fcf"];
+    var rulers = [], value = "";
+    for (var i = 1; i <= 1; i++) {
+        rulers.push({ color: colors[i], column: i * 10, lineStyle: "dashed" });
+        for (var j = 1; j < i; j++) value += space;
+            value += nums + "\n";
     }
-});
 
-gutter.addEventListener('mousedown', _ => gutter_drag = true);
-window.addEventListener('mouseup', _ => gutter_drag = false);
+    CodeMirror.commands.autocomplete = function (cm) {
+        cm.showHint();
+    }
+
+    var editor = CodeMirror.fromTextArea(document.querySelector("#code_editor"), {
+        lineNumbers: true,
+        styleActiveLine: true,
+        rulers: rulers,
+        value: value + value + value,
+        smartIndent: true,
+        matchBrackets: true,
+        indentWithTabs: true,
+        indentUnit: 4,
+        tabSize: 4,
+        autofocus: true,
+        mode: "text/x-csrc",
+        viewportMargin: Infinity,
+        electricChars: true,
+        autoCloseBrackets: true,
+        lineWrapping: true,
+        extraKeys: {
+            "Ctrl-Q": function (cm) { cm.foldCode(cm.getCursor()); },
+            "Ctrl-Space": "autocomplete"
+        },
+        foldGutter: true,
+        gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+        theme: "shadowfox"
+    });
+
+    var theme_selection = document.getElementById("theme_selection");
+    theme_selection.addEventListener("change", selectTheme);
+    function selectTheme(e) {
+        var theme = theme_selection.options[theme_selection.selectedIndex].textContent;
+        editor.setOption("theme", theme);
+        location.hash = "#" + theme;
+    }
+
+    const code_area = document.querySelector(".code_area");
+});
